d895aef2c2a16db452a8aec873befd532f3dca57
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2020-10-25 18:10:05 +0530

    Adding template system

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Blog
 
 # Create your views here.
 def index(request):
-    return HttpResponse('You are at the Blog Index')
+    blog_list = Blog.objects.order_by('-pub_date')
+    template = loader.get_template('blog/index.html')
+    context = {
+        'blog_list': blog_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def single(request, blog_id):
     return HttpResponse('You are viewing Blog number: {}'.format(blog_id))
