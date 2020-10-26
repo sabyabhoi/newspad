5485049ca7eeaf219e64b5d4dba6f880bb27a35b
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2020-10-26 11:48:55 +0530

    Added Rich text formatting to blog

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Blog(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     pub_date = models.DateTimeField('date published')
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
+    brief = models.TextField()
 
     def __str__(self):
         return self.title
