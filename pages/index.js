7e2fbdcdafaf708c6e73f9d3d9d26531ecb923ea
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-02-21 19:54:42 +0530

    Remove api files

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ const Home = () => {
     <div className='container mx-auto flex flex-col w-9/12'>
       <NavBar />
       <Head>
-        <title>Sabya's website</title>
+        <title>Sabya&apos;s website</title>
       </Head>
 
       <div className='flex flex-row justify-center'>
-        <h1 className='text-6xl font-bold py-2 my-10'>Hi, I'm Sabya.</h1>
+        <h1 className='text-6xl font-bold py-2 my-10'>Hi, I&apos;m Sabya.</h1>
       </div>
       <Section title='Work'>
-        I'm a second-year engineering student at Birla Institute of Technology
-        and Science. I'm a Linux enthusiast. I use GNU/Linux as my primary
+        I&apos;m a second-year engineering student at Birla Institute of Technology
+        and Science. I&apos;m a Linux enthusiast. I use GNU/Linux as my primary
         driver, and like to tinker around with new, free and open-source
         projects.
       </Section>
