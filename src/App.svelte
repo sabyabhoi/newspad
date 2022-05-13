deb60bf9d4c78bd97abbf0e5c448718c70c95900
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-05-14 00:08:30 +0530

    Switch to svelte

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+import Navbar from './components/Navbar.svelte'
+</script>
+
+<main>
+	<div>
+		<Navbar/>
+		<h1>Hi, I'm Sabya</h1>
+	</div>
+</main>
+
+<style>
+	main {
+		background-color: black;
+		color: orange;
+		height: 100vh;
+	}
+	div {
+		width: 80%;
+		margin-inline: auto;
+	}
+</style>
+
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script></script>
+
+<header>
+	<div>Sabyasachi Bhoi</div>
+	<div>change theme</div>
+</header>
+
+<style>
+	header {
+		padding-block: 1rem;
+		padding-inline: 0.5rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+</style>
