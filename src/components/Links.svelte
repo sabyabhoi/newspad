21cc686869f329fb8f54bb994ad58db8186a2642
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-05-14 12:50:41 +0530

    Refactor details into separate components

---
+++ src/components/Links.svelte
@@ -0,0 +1,36 @@
+<script>
+	import InfoSection from "./InfoSection.svelte";
+	import { Github, Linkedin, EnvelopeFill } from "svelte-bootstrap-icons";
+</script>
+
+<InfoSection title="On the Web">
+	<ul class="links">
+		<li>
+			<a href="https://github.com/sabyabhoi/"> <Github />@sabyabhoi</a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/in/sabyabhoi/">
+				<Linkedin />sabyabhoi</a
+			>
+		</li>
+		<li>
+			<a href="mailto:sabyabhoi@gmail.com"
+				><EnvelopeFill />sabyabhoi@gmail.com</a
+			>
+		</li>
+	</ul>
+</InfoSection>
+
+<style>
+	a {
+		color: white;
+		font-weight: bold;
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	.links {
+		list-style-type: none;
+	}
+</style>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,16 +1,40 @@
-<script></script>
+<script>
+	import { MoonFill, SunFill } from "svelte-bootstrap-icons";
+
+	let lightMode = false;
+</script>
 
 <header>
 	<div><b>Sabyasachi Bhoi</b></div>
-	<div>change theme</div>
+	<div>
+		<button>
+			{#if lightMode}
+				<MoonFill />
+			{:else}
+				<SunFill />
+			{/if}
+		</button>
+	</div>
 </header>
 
 <style>
 	header {
-		padding-block: 1rem;
+		margin-block: 1rem;
 		padding-inline: 0.5rem;
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
+		align-items: center;
+	}
+
+	button {
+		color: white;
+		background: none;
+		padding: 0.8rem;
+		border: none;
+		border-radius: 0.5rem;
+	}
+	button:hover {
+		background-color: orange;
 	}
 </style>
